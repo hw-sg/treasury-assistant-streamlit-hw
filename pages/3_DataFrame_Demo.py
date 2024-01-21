b90b7bd73d99149ae63ed049613aded421a2b7f4
hw-sg <54394299+hw-sg@users.noreply.github.com>
2024-01-21 09:24:33 +0800

    Update 3_DataFrame_Demo.py

--- pages/3_DataFrame_Demo.py
+++ pages/3_DataFrame_Demo.py
@@ -26,9 +26,18 @@ import pandas as pd
 
 df = pd.DataFrame(
     [
-        {"month": "Jan", "Date": 4,},
-        {"month": "Feb", "Date": 5,},
-        {"month": "Mar", "Date": 3, },
+        {"month": "Jan", "Date": 31,},
+        {"month": "Feb", "Date": ,},
+        {"month": "Mar", "Date": 20, },
+        {"month": "Apr", "Date": , },
+        {"month": "May", "Date": 1, },
+        {"month": "Jun", "Date": 12, },
+        {"month": "Jul", "Date": 31, },
+        {"month": "Aug", "Date": 20, },
+        {"month": "Sep", "Date": , },
+        {"month": "Oct", "Date": , },
+        {"month": "Nov", "Date": 7, },
+        {"month": "Dec", "Date": 18, },
     ]
 )
 
