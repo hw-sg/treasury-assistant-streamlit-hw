f16f751bacb66663c1d794719e7aefef02d1a305
hw-sg <54394299+hw-sg@users.noreply.github.com>
2024-01-21 21:12:20 +0800

    Update 4_Fed_Fund_Rate_History.py

--- pages/4_Fed_Fund_Rate_History.py
+++ pages/4_Fed_Fund_Rate_History.py
@@ -8,26 +8,25 @@ from streamlit.hello.utils import show_code
 
 import streamlit as st
 import pandas as pd
-
-from vega_datasets import data
+import plotly.express as px
 
 st.set_page_config(page_title="Fed Fund Rate History ", page_icon="📈")
 st.markdown("# Fed Fund Rate History ")
 st.write("""The chart below shows the Fed Fund Rate History.""")
 
-# assigning url to a variable
-url="https://github.com/hw-sg/treasury-assistant-streamlit-hw/blob/d5defa0c0ff536bd00026a04d3d2a0ce8290ae56/Fed_Rate.csv"
- 
-# passing parameter to the function
-source =pd.read_csv(url)
+# url="https://github.com/hw-sg/treasury-assistant-streamlit-hw/blob/d5defa0c0ff536bd00026a04d3d2a0ce8290ae56/Fed_Rate.csv"
+# source =pd.read_csv(url)
 
-#source = data.stocks()
 
-alt.Chart(source).mark_area(
-    color="lightblue",
-    interpolate='step-after',
-    line=True
-).encode(
-    x='Date',
-    y='Fed_Rate'
-)
+df = pd.read_csv("Fed_Rate.csv")
+st.dataframe(df)
+
+st.title("Fed Fund Rate History")
+
+contact_options = ["Fed_Rate", "Date"]
+contact_selected = st.selectbox("Select a Students value", contact_options)
+
+inform = f"Students {contact_selected} Chart:"
+fig = px.line(df, x="Name", y=contact_selected, title=inform)
+
+st.plotly_chart(fig, use_container_width=True)
