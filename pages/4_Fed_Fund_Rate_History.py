aaa1d7ce8b151e4a62fb1eb19addf3565129a08c
hw-sg <54394299+hw-sg@users.noreply.github.com>
2024-01-27 11:39:02 +0800

    Update 4_Fed_Fund_Rate_History.py

--- pages/4_Fed_Fund_Rate_History.py
+++ pages/4_Fed_Fund_Rate_History.py
@@ -20,6 +20,6 @@ st.dataframe(df)
 
 #st.title("Fed Fund Rate History")
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+#chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
-st.line_chart(chart_data)
+#st.line_chart(chart_data)
