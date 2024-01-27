5986cf8eecfe84e09c860430242df4d6926e45d2
hw-sg <54394299+hw-sg@users.noreply.github.com>
2024-01-27 13:17:29 +0800

    Update 4_Fed_Fund_Rate_History.py

--- pages/4_Fed_Fund_Rate_History.py
+++ pages/4_Fed_Fund_Rate_History.py
@@ -9,6 +9,14 @@ st.set_page_config(page_title="Fed Fund Rate History ", page_icon="📈")
 st.markdown("# Fed Fund Rate History ")
 st.write("""The chart below shows the Fed Fund Rate History.""")
 
+@st.cache_data
+def load_data():
+    df = pd.read_csv(file)
+    return df
+
+file = "./data/Fed_Rate.csv"
+df = load_data()
+
 #url="https://github.com/hw-sg/treasury-assistant-streamlit-hw/blob/d5defa0c0ff536bd00026a04d3d2a0ce8290ae56/Fed_Rate.csv"
 #df = pd.read_csv(url,index_col=0)
 #df = pd.read_csv("https://github.com/hw-sg/treasury-assistant-streamlit-hw/blob/d5defa0c0ff536bd00026a04d3d2a0ce8290ae56/Fed_Rate.csv", sep=",")
@@ -20,6 +28,6 @@ st.write("""The chart below shows the Fed Fund Rate History.""")
                    sheet_name=Sheet1,
                    usecols='A:B',
                    header=0)
-df = pd.read_csv("Fed_Rate.csv")
-#st.line_chart(df)
-st.dataframe(df)
+#df = pd.read_csv("Fed_Rate.csv")
+st.line_chart(df)
+#st.dataframe(df)
