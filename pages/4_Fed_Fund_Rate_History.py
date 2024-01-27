f0f81c664fc5b01bbe107d2b6a451860c5bddd3d
hw-sg <54394299+hw-sg@users.noreply.github.com>
2024-01-27 11:50:59 +0800

    Update 4_Fed_Fund_Rate_History.py

--- pages/4_Fed_Fund_Rate_History.py
+++ pages/4_Fed_Fund_Rate_History.py
@@ -11,11 +11,10 @@ st.write("""The chart below shows the Fed Fund Rate History.""")
 
 url="https://github.com/hw-sg/treasury-assistant-streamlit-hw/blob/d5defa0c0ff536bd00026a04d3d2a0ce8290ae56/Fed_Rate.csv"
 df = pd.read_csv(url,index_col=0)
-print(df.head(5))
 
 #df = pd.read_csv("https://github.com/hw-sg/treasury-assistant-streamlit-hw/blob/d5defa0c0ff536bd00026a04d3d2a0ce8290ae56/Fed_Rate.csv", sep=",")
 
-#st.dataframe(df)
+st.dataframe(df)
 
 #st.title("Fed Fund Rate History")
 
