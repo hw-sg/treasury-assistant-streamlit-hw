de7c988a3646544fe48ce612904285d295bb61f4
hw-sg <54394299+hw-sg@users.noreply.github.com>
2024-01-27 12:25:16 +0800

    Update 4_Fed_Fund_Rate_History.py

--- pages/4_Fed_Fund_Rate_History.py
+++ pages/4_Fed_Fund_Rate_History.py
@@ -13,12 +13,12 @@ st.write("""The chart below shows the Fed Fund Rate History.""")
 #df = pd.read_csv(url,index_col=0)
 #df = pd.read_csv("https://github.com/hw-sg/treasury-assistant-streamlit-hw/blob/d5defa0c0ff536bd00026a04d3d2a0ce8290ae56/Fed_Rate.csv", sep=",")
 
-excel_file = 'Fed_Rate.xlsx'
-sheet_name = 'Sheet1'
+#excel_file = 'Fed_Rate.xlsx'
+#sheet_name = 'Sheet1'
 
-df = pd.read_excel(excel_file,
+#df = pd.read_excel(excel_file,
                    sheet_name=Sheet1,
                    usecols='A:B',
                    header=0)
-
+df = pd.read_csv("Fed_Rate.csv")
 st.line_chart(df)
