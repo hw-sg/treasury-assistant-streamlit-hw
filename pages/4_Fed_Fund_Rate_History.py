3b46a28fa50b8b3d7fb6b5aef1c6da7a2ffea7e9
hw-sg <54394299+hw-sg@users.noreply.github.com>
2024-01-27 13:18:50 +0800

    Update 4_Fed_Fund_Rate_History.py

--- pages/4_Fed_Fund_Rate_History.py
+++ pages/4_Fed_Fund_Rate_History.py
@@ -1,6 +1,5 @@
 from urllib.error import URLError
 
-import altair as alt
 import pandas as pd
 import streamlit as st
 import numpy as np
@@ -20,14 +19,9 @@ df = load_data()
 #url="https://github.com/hw-sg/treasury-assistant-streamlit-hw/blob/d5defa0c0ff536bd00026a04d3d2a0ce8290ae56/Fed_Rate.csv"
 #df = pd.read_csv(url,index_col=0)
 #df = pd.read_csv("https://github.com/hw-sg/treasury-assistant-streamlit-hw/blob/d5defa0c0ff536bd00026a04d3d2a0ce8290ae56/Fed_Rate.csv", sep=",")
-
 #excel_file = 'Fed_Rate.xlsx'
 #sheet_name = 'Sheet1'
-
-#df = pd.read_excel(excel_file,
-                   sheet_name=Sheet1,
-                   usecols='A:B',
-                   header=0)
+#df = pd.read_excel(excel_file, sheet_name=Sheet1, usecols='A:B', header=0)
 #df = pd.read_csv("Fed_Rate.csv")
+
 st.line_chart(df)
-#st.dataframe(df)
