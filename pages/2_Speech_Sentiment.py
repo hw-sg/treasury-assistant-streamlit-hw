3c14c0393266e5b0f1325f742baa76a287d72095
hw-sg <54394299+hw-sg@users.noreply.github.com>
2024-03-09 20:05:33 +0800

    Update 2_Speech_Sentiment.py

--- pages/2_Speech_Sentiment.py
+++ pages/2_Speech_Sentiment.py
@@ -4,11 +4,11 @@ import numpy as np
 import streamlit as st
 
 
-st.set_page_config(page_title="Speech Sentiment", page_icon="📹")
-st.markdown("# Speech Sentiment")
-st.sidebar.header("Speech Sentiment")
+st.set_page_config(page_title="FED Speech Sentiment", page_icon="📹")
+st.markdown("# FED Speech Sentiment")
+st.sidebar.header("FED Speech Sentiment")
 st.write(
-    """This app shows the results of using Gen AI to perform sentiment analysis on FOMC speech to derive trading signals."""
+    """This app shows the results of using LLM to perform sentiment analysis on FED speeches to derive trading signals."""
 )
 st.image('sentiment.jpg', caption='Sentiment analysis of FOMC speech')
 
